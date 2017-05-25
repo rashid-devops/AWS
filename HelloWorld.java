0a4f56cd6d12331866fa337696239d9c82bb6beb
root <root@ip-172-31-12-207.ap-south-1.compute.internal>
2017-05-25 03:10:38 +0000

    new line

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,7 @@ public class HelloWorld
 public static void main(String[] args)
 {
 System.out.println("This program is run from Github");
+System.out.println("Build Pipeline created and this is upstream project");
 System.out.println("Hello Java World");
 }
 }
